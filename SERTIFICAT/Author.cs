6e9841257686d2c8ff3464aed4cda9857bd05d7d
JohnKey3 <JoJoKey3@users.noreply.github.com>
2021-02-18 20:06:05 +0300

    Добавьте файлы проекта.

---
+++ SERTIFICAT/Author.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SERTIFICAT
+{
+    public partial class Author : Form
+    {
+        public Author()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "123" && textBox2.Text == "123")
+            {
+                new Form1().Show();
+                this.Hide();
+            }
+        }
+    }
+}
---
+++ SERTIFICAT/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SERTIFICAT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+int i = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (textBox1.Text == "" && maskedTextBox1.Text == "" && textBox3.Text == "")
+            {
+                MessageBox.Show("Заполните всё");
+            }
+            else
+            {
+                i++;
+                dataGridView1.Rows.Add(i, textBox1.Text, maskedTextBox1.Text, textBox3.Text);
+            }
+            int b = dataGridView1.Rows.Count;
+            label4.Text = "Количетсов записей: " + (b- 1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.SelectedRows[0].Cells[1].Value = textBox1.Text;
+            dataGridView1.SelectedRows[0].Cells[2].Value = maskedTextBox1.Text;
+            dataGridView1.SelectedRows[0].Cells[3].Value = textBox3.Text;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                textBox1.Text = Convert.ToString(dataGridView1.SelectedRows[0].Cells[1].Value);
+                maskedTextBox1.Text = Convert.ToString(dataGridView1.SelectedRows[0].Cells[2].Value);
+                textBox3.Text = Convert.ToString(dataGridView1.SelectedRows[0].Cells[3].Value);
+                
+            }
+            catch
+            {
+                MessageBox.Show("Выбери корретную строку");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow item in this.dataGridView1.SelectedRows)
+            {
+                dataGridView1.Rows.RemoveAt(this.dataGridView1.SelectedRows[0].Index);
+
+
+                int b = dataGridView1.Rows.Count;
+                label4.Text = "Количетсов записей: " + (b - 1);
+            }
+        }
+    }
+}
